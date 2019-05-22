6b448285fa2cb0b4bd75c348e12db0a342d88386
Glitch (webauthn-codelab-resolution) <none>
2019-05-22 06:03:34 +0000

    😜⛄️ Checkpoint ./public/client.js:343052/170

--- public/client.js
+++ public/client.js
@@ -143,3 +143,8 @@ export const authenticate = async (opts) => {
 
   return await _fetch(`/auth/signinResponse`, credential);
 };
+
+export const unregisterCredential = async (credId) => {
+  localStorage.removeItem('credId');
+  return _fetch(`/auth/removeKey?credId=${encodeURIComponent(credId)}`);
+};
